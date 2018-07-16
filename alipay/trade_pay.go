e77583ab58e3c0d907190a4262a64a2fa4601b83
邓美玉 <dengmeiyu@zbj.com>
2018-07-16 15:27:23 +0800

    支付宝有毒UTF-8 2 utf-8

--- alipay/trade_pay.go
+++ alipay/trade_pay.go
@@ -161,7 +161,7 @@ func (tra *AliTrade) InitBaseConfig(config *BaseAliConfig) {
 
 	config.TimeStamp = time.Now().Format("2006-01-02 15:04:05")
 	config.Method = "alipay.trade.pay"
-	config.Charset = "UTF-8"
+	config.Charset = "utf-8"
 	config.Version = "1.0"
 	config.SignType = "RSA2"
 	tra.BaseAliConfig = config
