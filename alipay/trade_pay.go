c0108116192f0e07b985f08e5902639ffc0de23a
邓美玉 <dengmeiyu@zbj.com>
2018-07-16 10:13:27 +0800

    修改退款参数

--- alipay/trade_pay.go
+++ alipay/trade_pay.go
@@ -59,21 +59,21 @@ type AliTrade struct {
 	*AliConf
 }
 
-func (tra *AliTrade) Handle(conf map[string]interface{}, privateKey string, aliPublicKey string) (*AliPayTradePayResponse, error) {
+func (tra *AliTrade) Handle(conf map[string]interface{}, privateKey string, aliPublicKey string) (string, error) {
 
 	err := tra.BuildData(conf)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	ret, err := tra.sendReq(ALITRADE, tra, privateKey)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	fmt.Println(string(ret))
 	return tra.RetData(ret, aliPublicKey)
 }
 
-func (tra *AliTrade) RetData(ret []byte, aliPublicKey string) (re *AliPayTradePayResponse, err error) {
+func (tra *AliTrade) RetData(ret []byte, aliPublicKey string) (re string, err error) {
 
 	result := new(AliPayTradePayResponse)
 	json.Unmarshal(ret, result)
@@ -88,9 +88,9 @@ func (tra *AliTrade) RetData(ret []byte, aliPublicKey string) (re *AliPayTradePa
 	}
 
 	if result.AliPayTradePay.Code != "10000" && result.AliPayTradePay.Msg != "Success" {
-		return result, errors.New("支付宝退款失败：" + result.AliPayTradePay.SubMsg)
+		return "", errors.New("支付宝退款失败：" + result.AliPayTradePay.SubMsg)
 	}
-	return result, nil
+	return result.AliPayTradePay.TradeNo, nil
 
 }
 
